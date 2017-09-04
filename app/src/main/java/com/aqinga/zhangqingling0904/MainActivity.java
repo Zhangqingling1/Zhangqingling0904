c00172f45649594f36b36f6e5f348651ff76ea21
ZhangqinglingA <17600920523@189.cn>
2017-09-04 10:43:46 +0800

    Initial commit

---
+++ app/src/main/java/com/aqinga/zhangqingling0904/MainActivity.java
@@ -0,0 +1,17 @@
+package com.aqinga.zhangqingling0904;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
